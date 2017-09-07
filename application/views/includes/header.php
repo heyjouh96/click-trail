dec2639cbe22023662ac6fc30410f42a2251fb26
Jonathan Araujo <jnh.araujo@outlook.com>
2017-09-07 01:35:10 +0000

    layout inicial

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -3,6 +3,7 @@
     <head>
     
        <link href="<?= base_url() ?>assets/css/bootstrap.min.css" rel="stylesheet"> 
+       <link href="<?=base_url() ?>assets/css/style.css" rel="stylesheet">
     </head>
     
     <body>
--- application/views/index.php
+++ application/views/index.php
@@ -1,10 +1,39 @@
 
-        
-<h1>Click-trail</h1>
-        
-<p>index</p>
 
-<a href="<?= base_url() ?>paginas/painel">Painel</a>
+<!-- INICIO -->
+<div id="inicio">
+    <div class="container-fluid">
+        <div id="header">
+            <h1>Click-trail</h1>
+                    
+            <p>Contador de clicks</p>
+        </div>
+    </div>
+</div>    
+    
+<!--INFO PLUGIN-->
+<div class="container-fluid" transform: translateY(-13.3333px);opacity: 0.92;>
+    <div id="info">
+        <h2>Sobre o ClickTrail<h2>
+    </div>
+</div>
 
-<img src="<?= base_url() ?>assets/img/logo.png">
+
+<!--DOWNLOAD-->
+<div class="container-fluid">
+    <div id="download">
+        <h2>Como baixar<h2>
+    </div>
+</div>
+
+
+<!--SOBRE NÓS-->
+<div class="container-fluid">
+    <div id="about-us">
+        <h2>Sobre nós<h2>
+    </div>
+</div>
+    
+
+
 
