681a20fcc05ae8919b95d57ae060f3bdeec30607
Gustavo Oliveira <gustavo.oliveira141998@hotmail.com>
2017-10-16 05:46:35 +0000

    layout home pt2

--- application/views/index.php
+++ application/views/index.php
@@ -4,9 +4,32 @@
 <div id="inicio">
     <div class="container-fluid">
         <div id="header">
-            <h1>Click-trail</h1>
-                    
-            <p>Contador de clicks</p>
+            <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item">
+                            <a class="nav-link js-scroll-trigger" href="#login">Login</a>
+                        </li>
+                        
+                        <li class="nav-item">
+                            <a class="nav-link js-scroll-trigger" href="#cadastro">Cadastro</a>
+                        </li>
+                        
+                        <li class="nav-item">
+                            <a class="nav-link js-scroll-trigger" href="#painel">Painel</a>
+                        </li>
+                        
+                    </ul>
+                </div>
+            </nav>
+            
+            <div class="row">
+                <div class="col-md-12">
+                    <h1>Click-trail</h1>
+                            
+                    <p>Contador de clicks</p>
+                </div>
+            </div>
         </div>
     </div>
 </div>    
@@ -14,26 +37,74 @@
 <!--INFO PLUGIN-->
 <div class="container-fluid" transform: translateY(-13.3333px);opacity: 0.92;>
     <div id="info">
-        <h2>Sobre o ClickTrail<h2>
+        <h2>Sobre o ClickTrail</h2>
+        <hr class="bg-dark w-25 ml-1">
+        
+        <div class="row featurette edit">
+            <div class="col-md-5">
+              <img src="assets/img/logo.png">
+            </div>
+            
+            <div class="col-md-7">
+              <h3>Heading</h3>
+              <p class="lead">
+                  Donec id elit non mi porta gravida at eget metus. 
+                  Fusce dapibus, tellus ac cursus commodo, tortor 
+                  mauris condimentum nibh, ut fermentum massa justo 
+                  sit amet risus. Etiam porta sem malesuada magna 
+                  mollis euismod. Donec sed odio dui. 
+              </p>
+            </div>
+        </div>
     </div>
 </div>
-
 
 <!--DOWNLOAD-->
 <div class="container-fluid">
     <div id="download">
-        <h2>Como baixar<h2>
+        <h2>Como baixar</h2>
+        <hr class="bg-dark w-25 ml-1">
+            
+        <div class="row featurette edit">
+            <div class="col-md-5 order-md-2">
+              <img src="assets/img/logo.png">
+            </div>
+            
+            <div class="col-md-7">
+              <h3>Heading</h3>
+              <p class="lead">
+                  Donec id elit non mi porta gravida at eget metus. 
+                  Fusce dapibus, tellus ac cursus commodo, tortor 
+                  mauris condimentum nibh, ut fermentum massa justo 
+                  sit amet risus. Etiam porta sem malesuada magna 
+                  mollis euismod. Donec sed odio dui. 
+              </p>
+            </div>
+        </div>
     </div>
 </div>
+
+
+    
 
 
 <!--SOBRE NÓS-->
 <div class="container-fluid">
     <div id="about-us">
-        <h2>Sobre nós<h2>
+        <h2>Sobre nós</h2>
+        <hr class="bg-dark w-25 ml-1">
+        
+        <div class="row featurette about edit">
+            <div class="col-md-12">
+              <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
+              <h3>Heading</h3>
+              <p class="lead">
+                  Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. 
+                  Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus,
+                  porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.
+                  
+                </p>
+            </div>
+        </div>
     </div>
 </div>
-    
-
-
-
