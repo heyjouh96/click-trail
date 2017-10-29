3bcf2d5d7bee1f401e5ed4e56a304f6055850f8c
Gustavo Oliveira <gustavo.oliveira141998@hotmail.com>
2017-10-29 01:04:01 +0000

    layout cadastro

--- application/views/cadastro.php
+++ application/views/cadastro.php
@@ -19,36 +19,59 @@
           <?= $this->session->flashdata('sucesCadastro'); ?>
         </div>
     <?php } ?>
+</div>
     
-    
-    
-    
-    
-    <h1>Área de Usuários</h1>
     
     <!-- CADASTRO -->
-    <h2>Fazer Cadastro</h2>
-    <form action="<?= base_url() ?>cadastro/cadastrar" method="post" name="cadastro">
-        
-        <label for="nome">Nome</label>
-        <input type="text" name="nome" placeholder="Digite seu nome"> <br>
-        
-        <label for="nome">Sobrenome</label>
-        <input type="text" name="sbnome" placeholder="Digite seu sobrenome"> <br>
-        
-        <label for="email">E-mail</label>
-        <input type="text" name="email" placeholder="Digite seu e-mail"> <br>
-        
-        <label for="senha1">Senha</label>
-        <input type="password" name="senha1" placeholder="Digite sua senha"> <br>
-        
-        <label for="senha2">Confirmar Senha</label>
-        <input type="password" name="senha2" placeholder="Digite sua senha novamente"> <br>
-        
-        <input type="submit" value="Cadastrar">
-    </form>
+    <section class="login-block">
+      <div class="container">
+          <div class="row">
+              <div class="col-md-4 login-sec">
+                  <h2 class="text-center">Fazer Cadastro</h2>
+                  <form action="<?= base_url() ?>cadastro/cadastrar" method="post" name="cadastro" class="login-form">
+                      
+                      <div class="form-group">
+                          <label for="nome" class="text-uppercase">Nome</label>
+                          <input type="text" class="form-control" name="nome" placeholder="Digite seu nome"> <br>
+                      </div>
+                      
+                      <div class="form-group">
+                          <label for="nome" class="text-uppercase">Sobrenome</label>
+                          <input type="text" class="form-control" name="sbnome" placeholder="Digite seu sobrenome"> <br>
+                      </div>
+                      
+                      <div class="form-group">
+                          <label for="email" class="text-uppercase">E-mail</label>
+                          <input type="text" class="form-control" name="email" placeholder="Digite seu e-mail"> <br>
+                      </div>
+                      
+                      <div class="form-group">
+                          <label for="senha1" class="text-uppercase">Senha</label>
+                          <input type="password" class="form-control" name="senha1" placeholder="Digite sua senha"> <br>
+                      </div>
+                      <div class="form-group">
+                          <label for="senha2" class="text-uppercase">Confirmar Senha</label>
+                          <input type="password" class="form-control" name="senha2" placeholder="Digite sua senha novamente"> <br>
+                      </div>
+                      
+                      <div class="form-check">
+                          <label class="form-check-label">
+                              <input type="checkbox" class="form-check-input">
+                              <small>Manter-me conectado</small>
+                            </label>
+                          <button type="submit" class="btn btn-login float-right">Fazer Cadastro</button>
+                      </div>
+                  </form>
+              </div>
+              
+              <div class="col-md-8 banner-sec">
+                 <div class="banner-text">
+                    <h2>Click-Trail</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation</p>
+                 </div>		   
+        	    </div>
+          </div>
+      </div>
 
-</div>    
-
-
+    
 
--- application/views/login.php
+++ application/views/login.php
@@ -34,43 +34,43 @@
  
  <!-- LOGIN -->
     <section class="login-block">
-            <div class="container">
-            	<div class="row">
-            		<div class="col-md-4 login-sec">
-            		    
-            		    <h2 class="text-center">Login</h2>
-            		    
-            		    <form action="<?= base_url() ?>login/logar" method="post" name="login" class="login-form">
-                              <div class="form-group">
-                                  <label for="email" class="text-uppercase">E-mail</label>
-                                  <input type="text" class="form-control" name="email" placeholder="Digite seu e-mail"> <br>
-                                
-                              </div>
-                              
-                              <div class="form-group">
-                                  <label for="senha" class="text-uppercase">Senha</label>
-                                  <input type="password" class="form-control" placeholder="Digite sua senha">
-                              </div>
-                          
-                          
-                              <div class="form-check">
-                                  <label class="form-check-label">
-                                      <input type="checkbox" class="form-check-input">
-                                      <small>Manter-me conectado</small>
-                                    </label>
-                                  <button type="submit" class="btn btn-login float-right">Fazer Login</button>
-                              </div>
-                        </form>
-        		    </div>
+        <div class="container">
+        	<div class="row">
+        		<div class="col-md-4 login-sec">
         		    
-    		<div class="col-md-8 banner-sec">
-                 <div class="banner-text">
-                    <h2>Click-Trail</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation</p>
-                 </div>		   
-        	</div>
-    	</div>
-    
+        		    <h2 class="text-center">Login</h2>
+        		    
+        		    <form action="<?= base_url() ?>login/logar" method="post" name="login" class="login-form">
+                          <div class="form-group">
+                              <label for="email" class="text-uppercase">E-mail</label>
+                              <input type="text" class="form-control" name="email" placeholder="Digite seu e-mail"> <br>
+                            
+                          </div>
+                          
+                          <div class="form-group">
+                              <label for="senha" class="text-uppercase">Senha</label>
+                              <input type="password" class="form-control" placeholder="Digite sua senha">
+                          </div>
+                      
+                      
+                          <div class="form-check">
+                              <label class="form-check-label">
+                                  <input type="checkbox" class="form-check-input">
+                                  <small>Manter-me conectado</small>
+                                </label>
+                              <button type="submit" class="btn btn-login float-right">Fazer Login</button>
+                          </div>
+                    </form>
+    		    </div>
+    		    
+        		<div class="col-md-8 banner-sec">
+                     <div class="banner-text">
+                        <h2>Click-Trail</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation</p>
+                     </div>		   
+            	</div>
+	        </div>
+        </div>
 
 
 
