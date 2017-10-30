e83ea7bea2b3f2424fb78a7244f8b1ea0422f168
Gustavo Oliveira <gustavo.oliveira141998@hotmail.com>
2017-10-30 06:09:07 +0000

    layout painel

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -2,8 +2,12 @@
 <html lang="pt-br">
     <head>
     
+    <!--CSS-->
        <link href="<?= base_url() ?>assets/css/bootstrap.min.css" rel="stylesheet"> 
        <link href="<?=base_url() ?>assets/css/style.css" rel="stylesheet">
+       
+    
+        
     </head>
     
     <body>
--- application/views/painel/painel.php
+++ application/views/painel/painel.php
@@ -1,11 +1,77 @@
 
 
+    <nav class="navbar navbar-expand-md navbar-dark ">
+        <a class="navbar-brand" href="#">Click-trail</a>
+        <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
 
+        <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class= "nav-link glyphicon glyphicon-user" href="#" >Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Login</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Cadastro</a>
+            </li>
+          </ul>
+        </div>
+    </nav>
+    
+    
+    
+  <!--menu vertical-->  
+  
+  <div class="navi">
+      
+      <nav class="col-sm-3 col-md-3 side">
+          <h2 pn>Painel</h2>
+          <ul class="nav nav-pills flex-column">
+            <li class="nav-item active">
+              <a href="#"><span class="username"> <?= $usuario->nm_Usuario; ?> </span></a>
+            </li>
+            
+            
+            <li class="nav-item">
+              <a class="nav-link" href="<?= base_url() ?>cadastro/cadastrarSite">Cadastrar novo Site</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="<?= base_url() ?>login/logout">Sair</a>
+            </li>
+          </ul>
+  
+          
+      
+          <h2 pn>Domínios</h2>
+  
+          <ul class="nav nav-pills flex-column">
+            <li class="nav-item">
+              
+              <!-- mostra os sites cadastrados pelo usuário -->
+              <?php foreach($sites as $s){ ?>
+          
+                  <p> <a href="<?= base_url() ?>painel/siteInfo/<?= $s->cd_Site; ?>"> <?= $s->ds_Dominio; ?> </a></p>
+              
+              <?php } ?>    
+            </li>
+          </ul>
+      </nav>
+  </div>
+  
+  
+
+
+<!--
 <div class="container-fluid">      
+
+    
 
     <h1>Painel</h1>
     
-    <p> <?= $usuario->nm_Usuario; ?> </p>  
+    <p class="username"> <?= $usuario->nm_Usuario; ?> </p>  
     
     <p> <a href="<?= base_url() ?>cadastro/cadastrarSite">Cadastrar novo Site</a> </p>
     <p> <a href="<?= base_url() ?>login/logout">Sair</a> </p>
@@ -14,10 +80,12 @@
     
     <h2>Domínios</h2>
     
-    <!-- mostra os sites cadastrados pelo usuário -->
+    <!-- mostra os sites cadastrados pelo usuário
     <?php foreach($sites as $s){ ?>
         
         <p> <a href="<?= base_url() ?>painel/siteInfo/<?= $s->cd_Site; ?>"> <?= $s->ds_Dominio; ?> </a></p>
     
     <?php } ?>    
 </div>
+-->
+  <!--menu principal-->
