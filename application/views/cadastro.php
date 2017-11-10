6ee6b83b01c6fda87bf73406a9d0c737bfa89a21
Gustavo Oliveira <gustavo.oliveira141998@hotmail.com>
2017-11-10 03:30:24 +0000

    botão mostra senha

--- application/views/cadastro.php
+++ application/views/cadastro.php
@@ -1,3 +1,5 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
+
 <nav class="navbar navbar-expand-md navbar-dark ">
     <a class="navbar-brand" href="#">Click-trail</a>
     <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
@@ -66,7 +68,11 @@
                       
                       <div class="form-group">
                           <label for="senha1" class="text-uppercase">Senha</label>
-                          <input type="password" class="form-control" name="senha1" placeholder="Digite sua senha"> <br>
+                          <!--<input type="password" class="form-control" name="senha1" placeholder="Digite sua senha"> -->
+                          <input type="password" class="form-control" name="senha1" placeholder="Digite sua senha"id="senha">
+                          <img id="olho" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAABDUlEQVQ4jd2SvW3DMBBGbwQVKlyo4BGC4FKFS4+TATKCNxAggkeoSpHSRQbwAB7AA7hQoUKFLH6E2qQQHfgHdpo0yQHX8T3exyPR/ytlQ8kOhgV7FvSx9+xglA3lM3DBgh0LPn/onbJhcQ0bv2SHlgVgQa/suFHVkCg7bm5gzB2OyvjlDFdDcoa19etZMN8Qp7oUDPEM2KFV1ZAQO2zPMBERO7Ra4JQNpRa4K4FDS0R0IdneCbQLb4/zh/c7QdH4NL40tPXrovFpjHQr6PJ6yr5hQV80PiUiIm1OKxZ0LICS8TWvpyyOf2DBQQtcXk8Zi3+JcKfNafVsjZ0WfGgJlZZQxZjdwzX+ykf6u/UF0Fwo5Apfcq8AAAAASUVORK5CYII="/>
+                          
+                          <br>
                       </div>
                       <div class="form-group">
                           <label for="senha2" class="text-uppercase">Confirmar Senha</label>
@@ -88,5 +94,16 @@
           </div>
       </div>
 
-    
+      <script>
+      	var senha = $('#senha');
+      	var olho= $("#olho");
+      
+      	olho.mousedown(function() {
+      	  senha.attr("type", "text");
+      	});
+      
+      	olho.mouseup(function() {
+      	  senha.attr("type", "password");
+      	});
+      </script>
 
