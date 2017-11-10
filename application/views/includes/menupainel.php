6e9fe8cb255e6da2ca45cdb46bb4ca3418672265
Gustavo Oliveira <gustavo.oliveira141998@hotmail.com>
2017-11-10 03:23:17 +0000

    layout dominios

--- application/views/includes/menupainel.php
+++ application/views/includes/menupainel.php
@@ -2,13 +2,16 @@
     <a class="navbar-brand" href="#">Click-trail</a>
     <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
+      
+      
     </button>
 
     <div class="collapse navbar-collapse navbar-right" id="navbarsExampleDefault">
       <ul class="navbar-nav mr-auto">
         <li class="nav-item active">
-          <a class= "nav-link glyphicon glyphicon-user" href="#">Painel</a>
+          <a class= "nav-link" href="#">Painel</a>
         </li>
+        <span><i class="icono-user icone"></i></span>
       </ul>
     </div>
 </nav>
@@ -20,7 +23,7 @@
 
 <div class="row">
   
-    <div class="col-sm-2 col-md-2 side navi">
+    <div class="col-sm-2 col-md-2 side navi fixed">
       <nav>
           
           <ul class="nav nav-pills flex-column">
