beb837dbddf71a639b59d6189e6aabc7530f1f6a
Gustavo Oliveira <gustavo.oliveira141998@hotmail.com>
2017-11-19 03:19:12 +0000

    cadastrar sites layout

--- application/views/painel/cadastrar_site.php
+++ application/views/painel/cadastrar_site.php
@@ -1,5 +1,5 @@
 
-<main role="main" class="col-sm-9 ml-sm-auto col-md-10 pt-3">   
+<main role="main" class="col-sm-9 ml-sm-auto col-md-10 pt-3 login-block">   
       <!-- CADASTRO - MENSAGEM DE ERRO -->
       <?php if($this->session->flashdata('falhaCadastro')){ ?>
           <div class="alert alert-warning alert-dismissible show" role="alert">
@@ -21,22 +21,33 @@
       <?php } ?>
       
       
-      
-      <h1>Cadastrar Site</h1>
-      
-      <h2>Novo Site</h2>
-      <form action="<?= base_url() ?>site/cadastrar" method="post" name="cadastro">
-          
-          <label for="nome">Nome do Site</label>
-          <input type="text" name="nome" placeholder="Digite o nome do seu Website"> <br>
-          
-          <label for="dominio">Domínio</label>
-          <input type="text" name="dominio" placeholder="Digite o domínio do seu Website">
-          <small class="form-text text-muted">Ex: www.clicktrail.com.br</small>
-          
-          
-          <input type="submit" value="Cadastrar">
-          
-      </form>
-
+      <div class="container-fluid addsite login-sec">
+        <h1 style="border:none">Cadastrar Site</h1>
+        <div class="row">
+          <div class="col-lg-5 col-md-5 col-sm-5 login-block">
+            <div class="form-style">
+              
+              
+              
+              <form action="<?= base_url() ?>site/cadastrar" method="post" name="cadastro" class="form-control">
+                <h2 class="text-center">Novo Site</h2>
+                  <div class="form-group">
+                    <label for="nome" class="col-form-label">Nome do Site</label> <br>
+                    <input type="text" name="nome" placeholder="Digite o nome do seu Website" class="form-control"> <br>
+                  </div>
+                  
+                  <div class="form-group">
+                    <label for="dominio" class="col-form-label">Domínio</label>
+                    <input type="text" name="dominio" placeholder="Digite o domínio do seu Website" class="form-control">
+                    <small class="form-text text-muted">Ex: www.clicktrail.com.br</small>
+                  </div>
+                  
+                  <input type="submit" value="Cadastrar" class="btn btn-danger">
+                  
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
 </main>
+
